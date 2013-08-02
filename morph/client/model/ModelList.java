d68facee85185932b46f0a67b13b5c36641d3eb2
iChun <ohai.iChun@gmail.com>
2013-08-02 10:09:36 +0800

    Add ModelList class

---
+++ morph/client/model/ModelList.java
@@ -0,0 +1,44 @@
+package morph.client.model;
+
+import java.util.HashMap;
+
+import javax.swing.text.html.parser.Entity;
+
+import net.minecraft.entity.EntityLivingBase;
+
+public class ModelList 
+{
+
+	private static final HashMap<Class, ModelInfo> modelInfoMap = new HashMap<Class, ModelInfo>();
+	
+	public static void addModelInfo(Class clz, ModelInfo info)
+	{
+		if(EntityLivingBase.class.isAssignableFrom(clz))
+		{
+			modelInfoMap.put(clz, info);
+		}
+	}
+	
+	public static ModelInfo getModelInfo(Class clz)
+	{
+		ModelInfo info = modelInfoMap.get(clz);
+		if(info == null)
+		{
+			try
+			{
+				Class clzz = clz.getSuperclass();
+				while(clzz != Entity.class && Entity.class.isAssignableFrom(clzz) && info == null)
+				{
+					info = modelInfoMap.get(clzz);
+					clzz = clzz.getSuperclass();
+				}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		return info;
+	}
+	
+}
