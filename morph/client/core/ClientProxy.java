4638b4735bc7f7aba498102743a6ba00d959da3f
iChun <ohai.iChun@gmail.com>
2013-08-04 00:54:33 +0800

    Fix mod mobs support

--- morph/client/core/ClientProxy.java
+++ morph/client/core/ClientProxy.java
@@ -1,14 +1,20 @@
 package morph.client.core;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import morph.client.model.ModelInfo;
 import morph.client.model.ModelList;
 import morph.common.core.CommonProxy;
 import morph.common.core.ObfHelper;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderEntity;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
-import net.minecraft.entity.player.EntityPlayer;
+import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.ObfuscationReflectionHelper;
 import cpw.mods.fml.common.registry.TickRegistry;
 import cpw.mods.fml.relauncher.Side;
@@ -21,13 +27,53 @@ public class ClientProxy extends CommonProxy
 	{
 		super.initPostMod();
 		
+		HashMap<Class, RendererLivingEntity> renders = new HashMap<Class, RendererLivingEntity>();
+		try
+		{
+			List entityRenderers = (List)ObfuscationReflectionHelper.getPrivateValue(RenderingRegistry.class, RenderingRegistry.instance(), "entityRenderers");
+			
+			for(Object obj : entityRenderers)
+			{
+				Field[] fields = obj.getClass().getDeclaredFields();
+				Render render = null;
+				Class clzz = null;
+				for(Field f : fields)
+				{
+					f.setAccessible(true);
+					if(f.getType() == Render.class)
+					{
+						render = (Render)f.get(obj);
+					}
+					else if(f.getType() == Class.class)
+					{
+						clzz = (Class)f.get(obj);
+					}
+				}
+				if(render instanceof RendererLivingEntity && clzz != null)
+				{
+					renders.put(clzz, (RendererLivingEntity)render);
+				}
+			}
+		}
+		catch(Exception e)
+		{
+		}
+			
 		for(int i = compatibleEntities.size() - 1; i >= 0; i--)
 		{
 			Render rend = RenderManager.instance.getEntityClassRenderObject(compatibleEntities.get(i));
-			System.out.println(rend);
+			if(rend.getClass() == RenderEntity.class)
+			{
+				rend = renders.get(compatibleEntities.get(i));
+			}
 			if(!(rend instanceof RendererLivingEntity))
 			{
 				compatibleEntities.remove(i);
+				continue;
+			}
+			if(!renders.containsKey(compatibleEntities.get(i)))
+			{
+				renders.put(compatibleEntities.get(i), (RendererLivingEntity)rend);
 			}
 		}
 		
@@ -35,7 +81,7 @@ public class ClientProxy extends CommonProxy
 		{
 			try
 			{
-				RendererLivingEntity rend = (RendererLivingEntity)RenderManager.instance.getEntityClassRenderObject(clz);
+				RendererLivingEntity rend = (RendererLivingEntity)renders.get(clz);
 				ModelList.addModelInfo(clz, new ModelInfo(clz, rend, (ModelBase)ObfuscationReflectionHelper.getPrivateValue(RendererLivingEntity.class, rend, ObfHelper.mainModel)));
 			}
 			catch(Exception e)
